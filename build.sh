3789fef6803f1f7fa988af75be760d609efd3b0f
Xun Li <xunli@uchicago.edu>
2021-07-28 11:04:07 -0700

    add github action

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ mkdir -p build
 cd build
 emcmake cmake .. -DUSE_EMCC=True -DCMAKE_C_COMPILER=emcc -DCMAKE_CXX_COMPILER=em++ -DCMAKE_CPP_COMPILER=em++
 emmake make jsgeoda
-cp jsgeoda.js ~/Github/jsgeoda/src/jsgeoda.js
-cp jsgeoda.wasm ~/Github/jsgeoda/src/jsgeoda.wasm
+#cp jsgeoda.js ~/Github/jsgeoda/src/jsgeoda.js
+#cp jsgeoda.wasm ~/Github/jsgeoda/src/jsgeoda.wasm
 
